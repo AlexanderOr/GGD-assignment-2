ba16607be5ba60a84f5f7a2390cd69058853fc49
AlexanderOr <aortwine@ltu.edu>
2022-02-12 21:28:11 -0500

    Almost everything done

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject main;
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void optionsmenu()
+    {
+        credits.SetActive(true);
+        main.SetActive(false);
+    }
+    public void mainmenu()
+    {
+        credits.SetActive(false);
+        main.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
